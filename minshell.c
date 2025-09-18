71b0ce78a9a70fee831143cb4d4513f99b01227f
Amardeep Saha <amardeep.saha@indiamart.com>
2025-09-18 10:08:37 +0530

    shell is working now : 1. handling sigterm sigstop sigcont  in child processes

--- minshell.c
+++ minshell.c
@@ -5,62 +5,92 @@
 #include<stdlib.h>
 #include<sys/wait.h>
 #include<stdio.h>
-
-char** argumentParser(char *s){
-    char **argumentList = (char *)malloc(50);
-    char *str = NULL;
-
-    int i = 0;
-    int argumentCounter = 0;
-    int breakIdx  = 0;
-    while(s!=NULL && *(s+i)!='\0'){
-        if(!str){
-            str=(char)malloc(127);
-        }else{
-
-            if(*(s+i)==' '){
-                breakIdx = i;
-                str[i+1] = '\0';
-                argumentList[argumentCounter++]=str;
-                str =NULL;
-            }else{
-                str[i-(breakIdx+1)] = *(s+i);  
-            }
-            i++;
-        }
-    }
-
-    if(str!=NULL){
-        argumentList[argumentCounter++]=str;
-        str = NULL;
-    }
+#include<string.h>
+#include<unistd.h>
 
 
+// char** argumentParser(char *s, size_t *argumentCounter){
+//     if(s == NULL)return NULL;
+//     char **argumentList = (char **)malloc(50 * sizeof(char*));
+//     char *str = NULL;
 
-    return argumentList;
-}
+//     int i = 0;
+    
+//     int breakIdx  = -1;
 
+//     while(*(s+i)!='\0'){
+//         if(!str){
+//             str=(char*)malloc(127 * sizeof(char));
+//         }else{
+//             if(*(s+i)==' ' || *(s+i)=='\0' ){
+//                 breakIdx = i;
+//                 str[i+1] = '\0';
+//                 argumentList[*argumentCounter]=str;
+//                 *argumentCounter+=1;
+//                 free(str);
+//             }else{
+//                 str[i-(breakIdx+1)] = *(s+i);  
+//             }
+//             i++;
+//         }
+//     }
+
+//     if(str!=NULL){
+//         argumentList[*argumentCounter]=str;
+//         free(str);
+//         *argumentCounter+=1;
+//     }
+//     return argumentList;
+// }
+//use strtok
 
 int main(int argc , char *argv[]){
     // 1 create  a child process
-    // int child_p = fork();
-    // if(child_p == -1){
-    //     // error exit : process creation failed
-    // }else if(child_p == 0){
-    //     // run a infinite loop to listen to commands and args
-    //      for(;;){
-    //         // take a line in input:
-    //         // assume arg 0 as the program 
-    //      }
-    // }else{
-    //     // parent process;
-    // }
-    char *s;
-    scanf("%s",s);
-    char **argumentList = argumentParser(s);
-    printf("%ld",sizeof(argumentList)/sizeof(char*));
-    for(int i = 0 ; i< sizeof(argumentList)/sizeof(char*) ; i++){
-        printf("%s\n",*(argumentList+i));
+    
+    for(;;){
+        int child_p = fork();
+            if(child_p == -1){
+                // error exit : process creation failed
+            }else if(child_p == 0){
+                // run a infinite loop to listen to commands and args
+                    char *input_str = (char*)malloc(100 * sizeof(char));
+                    if(input_str == NULL){
+                        printf("unable to assign memory");
+                        exit(-1);
+                    }
+                
+                    if(fgets(input_str,100,stdin) != NULL){
+                        const char *reject_str = "\n";
+                        strcspn(input_str,reject_str);
+                    } 
+                
+                    // char **argumentList = (char**)malloc(100*sizeof(char*)); 
+                    // const char *delim = " ";
+                    // char *token = strtok(input_str,delim);
+
+                    // size_t arg_count = 0;
+                
+                
+                    // for(int i = 0 ; token!= NULL ; i++){
+                    //     *(argumentList+i) = token;
+                    //     ++arg_count;
+                    //     token = strtok(NULL,delim);
+                    // }
+                
+                    execl("/bin/sh", "sh" , "-c" ,input_str, (char*)NULL);
+                
+                    // if(argumentList!=NULL){
+                    //     free(argumentList);
+                    // }
+                
+                    if(input_str!=NULL){
+                        free(input_str);
+                    }
+            }else{
+                // parent process;
+                wait(NULL);
+                
+            }
+
     }
-    return 0;
 }
